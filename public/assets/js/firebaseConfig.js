844810fa6f251a0cf4865a36e009d99022dce0eb
Gary Baker <garybaker@garys-macbook.local>
2018-06-03 15:55:01 +1200

    Moved firebase details and changed the config for replication

---
+++ public/assets/js/firebaseConfig.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDxhseyCYnLTUJ3G9Pi3h3L1F5FA7ipQIA",
+    authDomain: "garybaker-96ace.firebaseapp.com",
+    databaseURL: "https://garybaker-96ace.firebaseio.com",
+    projectId: "garybaker-96ace",
+    storageBucket: "garybaker-96ace.appspot.com",
+    messagingSenderId: "1051095014648"
+};
+
+firebase.initializeApp(config);
